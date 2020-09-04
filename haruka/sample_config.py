d5088fce3b34bd17b2a8adbfffbf394da09b9eca
TITANHACKY <66989280+TITANHACKY@users.noreply.github.com>
2020-09-04 21:16:33 +0530

    Update sample_config.py

--- haruka/sample_config.py
+++ haruka/sample_config.py
@@ -10,9 +10,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "1380255961:AAEMVDZQyZnv-aMSCGSKbKSZc1lgArgNGhA"
+    OWNER_ID = "1037166796"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "TITANHACKY"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
